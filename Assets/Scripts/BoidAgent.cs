e58f76c321a75151e17d4e3062825e08d99501cb
SmolConnor <77941492+SmolConnor@users.noreply.github.com>
2023-05-01 16:39:13 +0100

    Boids
    we got moving boids just have to stop them from moving in ways i dont like

--- Assets/Scripts/BoidAgent.cs
+++ Assets/Scripts/BoidAgent.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class BoidAgent : MonoBehaviour
 {
-   
+    float speed;
     // Start is called before the first frame update
     void Start()
     {
-       
+        speed = Random.Range(Boidtest.boidManager.minSpeed, Boidtest.boidManager.maxSpeed);
     }
 
-    void Sepperation()
+
+
+  
+
+    // Update is called once per frame
+    void Update()
     {
+        
+        ApplyRules();
+        this.transform.Translate(0, speed*Time.deltaTime, 0);
+      
+        
+    }
+    void ApplyRules()
+    {
+        Vector3 vcentre = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.01f;
+        int groupSize = 0;
         GameObject[] AI;
         AI = Boidtest.boidManager.boids;
         foreach (GameObject boy in AI)
@@ -20,18 +37,27 @@ public class BoidAgent : MonoBehaviour
             if (boy != this.gameObject)
             {
                 float distance = Vector2.Distance(boy.transform.position, this.transform.position);
-                if (distance <= Boidtest.boidManager.spaceBetween)
+                if (distance <= Boidtest.boidManager.neighborD)
                 {
-                    Vector2 direction = transform.position - boy.transform.position;
-                    transform.Translate(direction * Time.deltaTime);
+                    vcentre += boy.transform.position;
+                    groupSize++;
+
+                    if (distance < 1.0f)
+                    {
+                        vavoid = vavoid + (this.transform.position - boy.transform.position);
+                    }
+
+                    BoidAgent otheAgent = boy.GetComponent<BoidAgent>();
+                    gSpeed = gSpeed + otheAgent.speed;
                 }
             }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Sepperation();
+        if (groupSize > 0)
+        {
+            vcentre = vcentre / groupSize;
+            speed = gSpeed / groupSize;
+            Vector2 direction2 = (vcentre + vavoid) - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction2), Boidtest.boidManager.rotation * Time.deltaTime);
+        }
     }
 }
--- Assets/Scripts/Boidtest.cs
+++ Assets/Scripts/Boidtest.cs
@@ -9,14 +9,25 @@ public class Boidtest : MonoBehaviour
     public int numBoid;
     public static Boidtest boidManager;
     public float spaceBetween = 1.5f;
-    
+    public Vector2 spawnLimits = new Vector2(5, 5);
+    [Header("the boys")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed;
+    [Range(1.0f, 10.0f)]
+    public float neighborD;
+    [Range(1.0f, 5.0f)]
+    public float rotation;
+
     // Start is called before the first frame update
     void Start()
     {
         boids = new GameObject[numBoid];
         for (int i = 0; i < numBoid; i++)
         {
-            boids[i] = Instantiate(boidAgent, this.transform.position, this.transform.rotation);
+            Vector2 pos = (Vector2)this.transform.position + new Vector2(Random.Range(-spawnLimits.x, spawnLimits.x), Random.Range(-spawnLimits.y, spawnLimits.y));
+            boids[i] = Instantiate(boidAgent, pos, this.transform.rotation);
         }
         boidManager = this;
     }
